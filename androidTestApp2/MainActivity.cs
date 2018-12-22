49bb0c5b8978cd784c5b5b741f441161e0f72dec
Kosyakoff <Kosyakoff@CUCUMBER>
2018-12-22 22:25:46 +0700

    Added support for second screen and history

--- androidTestApp2/MainActivity.cs
+++ androidTestApp2/MainActivity.cs
@@ -1,13 +1,28 @@
-﻿using Android.App;
-using Android.Widget;
-using Android.OS;
-using Android.Support.V7.App;
-
-namespace androidTestApp2
+﻿namespace androidTestApp2
 {
+    using System.Collections.Generic;
+
+    using Android.App;
+    using Android.Content;
+    using Android.OS;
+    using Android.Support.V7.App;
+    using Android.Widget;
+
+    using Core;
+
+    using Resources.layout;
+
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        #region Fields
+
+        private static readonly List<string> _phoneNumbers = new List<string>();
+
+        #endregion
+
+        #region Methods
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -15,13 +30,14 @@ namespace androidTestApp2
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
-            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
-            TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneword);
-            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            var phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            var translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneword);
+            var translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            var translationHistoryButton = FindViewById<Button>(Resource.Id.TranslationHistoryButton);
 
             translateButton.Click += (sender, e) =>
             {
-                string translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                string translatedNumber = PhonewordTranslator.ToNumber(phoneNumberText.Text);
 
                 if (string.IsNullOrWhiteSpace(translatedNumber))
                 {
@@ -30,9 +46,19 @@ namespace androidTestApp2
                 else
                 {
                     translatedPhoneWord.Text = translatedNumber;
+                    _phoneNumbers.Add(translatedNumber);
+                    translationHistoryButton.Enabled = true;
                 }
             };
+
+            translationHistoryButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(TranslationHistoryActivity));
+                intent.PutStringArrayListExtra("phone_numbers", _phoneNumbers);
+                StartActivity(intent);
+            };
         }
+
+        #endregion
     }
 }
-
---
+++ androidTestApp2/Resources/layout/TranslationHistoryActivity.cs
@@ -0,0 +1,25 @@
+﻿namespace androidTestApp2.Resources.layout
+{
+    using System.Collections.Generic;
+
+    using Android;
+    using Android.App;
+    using Android.OS;
+    using Android.Widget;
+
+    [Activity(Label = "@string/translationHistory")]
+    public class TranslationHistoryActivity : ListActivity
+    {
+        #region Methods
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            IList<string> phoneNumbers = Intent.Extras.GetStringArrayList("phone_numbers") ?? new string[0];
+            ListAdapter = new ArrayAdapter<string>(this, Resource.Layout.SimpleListItem1, phoneNumbers);
+        }
+
+        #endregion
+    }
+}
